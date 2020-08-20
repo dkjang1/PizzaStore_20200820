ba5b39fb7884d08f1b29d82ebdce9a445eaf61b7
Jinkyeong96 <69335959+Jinkyeong96@users.noreply.github.com>
2020-08-20 22:23:13 +0900

    전화걸기 숙제

--- app/src/main/java/com/kakao/pizzastore_20200820/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/kakao/pizzastore_20200820/ViewStoreDetailActivity.kt
@@ -28,7 +28,6 @@ class ViewStoreDetailActivity : BaseActivity() {
         nameTxt.text = mStore.name
         phoneNumTxt.text = mStore.phoneNum
         Glide.with(mContext).load(mStore.logoUrl).into(logoImg)
-
     }
 
 }
