edbe7f1e9da9dcabd6ae5f410a304a375d3304f3
gumsu <chavi9@naver.com>
2020-08-24 19:59:55 +0900

    닉네임변경화면 추가

--- app/src/main/java/com/kakao/pizzastore_20200820/ViewStoreDetailActivity.kt
+++ app/src/main/java/com/kakao/pizzastore_20200820/ViewStoreDetailActivity.kt
@@ -1,10 +1,16 @@
 package com.kakao.pizzastore_20200820
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
+import com.gun0912.tedpermission.TedPermission
 import com.kakao.pizzastore_20200820.datas.Store
 import kotlinx.android.synthetic.main.activity_view_store_detail.*
+import java.util.jar.Manifest
 
 //15:피자가게상세페이지(Intent) -> 16:피자가게전화걸기
 class ViewStoreDetailActivity : BaseActivity() {
@@ -20,6 +26,33 @@ class ViewStoreDetailActivity : BaseActivity() {
     }
 
     override fun setupEvents() {
+
+        //16-1:Manifest + Tedpermission
+        //16-2:CALL_PHONE
+        callBtn.setOnClickListener {
+
+            val permissionListener = object : PermissionListener {
+                //승인되었을경우
+                override fun onPermissionGranted() {
+                    //val myUri = Uri.parse("tel:${phoneNumTxt.text}")
+                    val myUri = Uri.parse("tel:${mStore.phoneNum}")
+                    val myIntent = Intent(Intent.ACTION_CALL, myUri)
+                    startActivity(myIntent)
+                }
+
+                //거부되었을경우
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+                    Toast.makeText(mContext, "전화연결 권한이 거부되었습니다.", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            TedPermission.with(mContext)
+            //    .setPermissions(permissionListener)
+                .setDeniedMessage("[설정]에서 권한을 확인해야 전화연결이 가능합니다,")
+                .setPermissions(android.Manifest.permission.CALL_PHONE)
+                .check()
+        }
     }
 
     override fun setValues() {
--- app/src/main/java/com/kakao/pizzastore_20200820/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/kakao/pizzastore_20200820/fragments/MyProfileFragment.kt
@@ -1,11 +1,14 @@
 package com.kakao.pizzastore_20200820.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.kakao.pizzastore_20200820.EditNicknameActivity
 import com.kakao.pizzastore_20200820.R
+import kotlinx.android.synthetic.main.fragment_my_profile.*
 
 //6:내정보변경 -> 7:TabLayout(StorePagerAdapter.kt)
 class MyProfileFragment : Fragment(){
@@ -23,6 +26,13 @@ class MyProfileFragment : Fragment(){
     //6-2:onActivityCreated
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        //17:nickname
+        changeNicknameBtn.setOnClickListener {
+
+            val myIntent = Intent(context!!, EditNicknameActivity::class.java)
+        }
+
     }
 
 }
